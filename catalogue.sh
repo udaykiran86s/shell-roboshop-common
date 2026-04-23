07c590cf26b89a9e9df57a3949ac7d27c8998107
unknown <samanthulaudaykiran@gmail.com>
2026-04-23 12:20:23 +0530

    messages

--- catalogue.sh
+++ catalogue.sh
@@ -14,12 +14,19 @@ dnf install mongodb-mongosh -y &>>$LOG_FILE
 VALIDATE $? "Install MongoDB client"
 
 
-INDEX=$(mongosh --host $MONGODB_HOST --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+# INDEX=$(mongosh --host $MONGODB_HOST --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
+# if [ $INDEX -le 0 ]; then
+#     mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
+#     VALIDATE $? "Load app_name products"
+# else
+#     echo -e "app_name products already loaded ... $Y SKIPPING $N"
+# fi
+INDEX=$(mongosh $MONGODB_HOST --quiet --eval "db.getMongo().getDBNames().indexOf('catalogue')")
 if [ $INDEX -le 0 ]; then
     mongosh --host $MONGODB_HOST </app/db/master-data.js &>>$LOG_FILE
-    VALIDATE $? "Load app_name products"
+    VALIDATE $? "Load catalogue products"
 else
-    echo -e "app_name products already loaded ... $Y SKIPPING $N"
+    echo -e "Catalogue products already loaded ... $Y SKIPPING $N"
 fi
 
 app_restart
