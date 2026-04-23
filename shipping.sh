c8877d71400bd0aa75e48fe72c71e3f570098c62
unknown <samanthulaudaykiran@gmail.com>
2026-04-23 14:05:14 +0530

    messages

--- shipping.sh
+++ shipping.sh
@@ -12,15 +12,25 @@ systemd_setup
 MYSQL_HOST=mysql.udaykiran.site
 dnf install mysql -y  &>>$LOG_FILE
  
+MYSQL_HOST=${MYSQL_HOST:-mysql.udaykiran.site}
+MYSQL_PASS="RoboShop@1"
 
-mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities' &>>$LOG_FILE
-if [ $? -ne 0 ]; then
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql  &>>$LOG_FILE
-    mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
+if mysql -h $MYSQL_HOST -uroot -p"$MYSQL_PASS" -e "use cities" &>>$LOG_FILE; then
+    echo -e "Shipping data is already loaded ... $y SKIPPING $n"
 else
-    echo -e "Shipping data is already loaded ... $Y SKIPPING $N"
+    mysql -h $MYSQL_HOST -uroot -p"$MYSQL_PASS" < /app/db/schema.sql &>>$LOG_FILE
+    mysql -h $MYSQL_HOST -uroot -p"$MYSQL_PASS" < /app/db/app-user.sql &>>$LOG_FILE
+    mysql -h $MYSQL_HOST -uroot -p"$MYSQL_PASS" < /app/db/master-data.sql &>>$LOG_FILE
 fi
+# mysql -h $MYSQL_HOST -uroot -pRoboShop@1 -e 'use cities' &>>$LOG_FILE
+# if mysql -h $MYSQL_HOST -uroot -p"$MYSQL_PASS" -e "use cities" &>>$LOG_FILE; then
+# if [ $? -ne 0 ]; then
+#     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/schema.sql &>>$LOG_FILE
+#     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/app-user.sql  &>>$LOG_FILE
+#     mysql -h $MYSQL_HOST -uroot -pRoboShop@1 < /app/db/master-data.sql &>>$LOG_FILE
+# else
+#     echo -e "Shipping data is already loaded ... $Y SKIPPING $N"
+# fi
 
 app_restart
 print_total_time
